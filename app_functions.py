6fd99a4c7a112f438e171e488452646197660ffe
chr1s <cgjimenez4@up.edu.ph>
2022-05-30 21:43:05 +0800

    FIX: fix generateTaskId, fix inputDeadline to allow input of today

--- app_functions.py
+++ app_functions.py
@@ -3,6 +3,7 @@ from queue import Empty
 import random
 import mysql.connector as mariadb
 from datetime import datetime
+from datetime import timedelta
 
 def clearTerminal():  # Clear the terminal.
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -34,79 +35,80 @@ def printMenu():    # Function for printing main menu where the user can choose 
     input1 = input('\nInput (1-11): ')
     return input1
 
-def generateTaskId(cursor):     # Function for generating a unique task ID that's 10 characters long
-    while(1):                   # Infinite loop
+def generateTaskId(cursor):                                                     # Function for generating a unique task ID that's 10 characters long
+    while(1):                                                                   # Infinite loop
         taskId = ''
         for i in range(10):
-            integer = random.randint(0, 9)      # Generates a random integer from 0-9
-            taskId += (str(integer))            # Adds them together in one string
-
+            integer = random.randint(0, 9)                                      # Generates a random integer from 0-9
+            taskId += (str(integer))                                            # Adds them together in one string
+            
         cursor.execute('SELECT taskid FROM task')  
-        if len(cursor.fetchall()) == 0:         # If there are no tasks,
-            print("\nTask ID:",taskId)          # print and return taskId
+        if len(cursor.fetchall()) == 0:                                         # If there are no tasks,
+            print("\nTask ID:",taskId)                                          # print and return taskId
             return "'"+taskId+"'"
 
-        for taskId1 in cursor:                  # If taskId already exists in tasks,
-            if taskId not in taskId1:           # Generate another one, else print and return
+        cursor.execute('SELECT taskid FROM task')      
+        for taskId1 in cursor:                                                  # If taskId already exists in tasks,
+            if taskId not in taskId1:                                           # Generate another one, else print and return
                 print("\nTask ID:",taskId)
                 return "'"+taskId+"'"
 
-def inputTask():                                                # Function for asking user input for their task description
-    while(1):                                                   # Infinite loop
-        task = input("\nAdd task description: ")                # User input
-        if len(task) > 30:                                      # If task description is longer than 30 characters, ask for input again.
+def inputTask():                                                                # Function for asking user input for their task description
+    while(1):                                                                   # Infinite loop
+        task = input("\nAdd task description: ")                                # User input
+        if len(task) > 30:                                                      # If task description is longer than 30 characters, ask for input again.
             print('Task cannot be longer than 30 characters. Try again.')
         else:
-            return "'"+task+"'"                                 # Else return it
+            return "'"+task+"'"                                                 # Else return it
 
-def inputDeadline():                                            # Function for asking user input for task deadline
-    format = "%Y-%m-%d"                                         # Date format is yyyy-mm-dd
-    present = datetime.now()                                    # Date today
-    while(1):                                                   # Infinite loop
+def inputDeadline():                                                            # Function for asking user input for task deadline
+    format = "%Y-%m-%d"                                                         # Date format is yyyy-mm-dd
+    yesterday = datetime.now() - timedelta(days = 1)                          # Date today
+    while(1):                                                                   
         deadline = input("\nAdd deadline (Leave blank if none, format is yyyy-mm-dd): ")
 
-        if deadline == '':                                      # If there is no input,
-            deadline = 'NULL'.strip(' ')                        # assume task has no deadline and return string 'NULL'
+        if deadline == '':                                                      # If there is no input,
+            deadline = 'NULL'.strip(' ')                                        # assume task has no deadline and return string 'NULL'
             return deadline
-        else:                                                   # Else
-            try:                                                # Check if input string follows correct format
+        else:                                                                   # Else
+            try:                                                                # Check if input string follows correct format
                 res = bool(datetime.strptime(deadline, format))
             except ValueError:
                 res = False
 
-            if not res:                                         # If format is incorrect, ask for input again.
+            if not res:                                                         # If format is incorrect, ask for input again.
                 print('Invalid date format!')
-            else:                                               # Else check if date is in the past. If it is, ask for input again.
-                if datetime.strptime(deadline,format) < present:
+            else:                                                               # Else check if date is in the past. If it is, ask for input again.
+                if datetime.strptime(deadline,format) < yesterday:
                     print('Date cannot be in the past!')
-                else:                                           # Else return the provided deadline
+                else:                                                           # Else return the provided deadline
                     return "'"+deadline+"'"
 
-def inputWillStart():                                           # Function to ask user if the task will be started today
+def inputWillStart():                                                           # Function to ask user if the task will be started today
     while(1):
         willStart = input("\nAre you going to start working on the task today? (Y/N): ")
-        if willStart == 'Y' or willStart == 'y':                # If yes,
-            dateStarted = 'CURDATE()'                           # dateStarted will be today
+        if willStart == 'Y' or willStart == 'y':                                # If yes,
+            dateStarted = 'CURDATE()'                                           # dateStarted will be today
             return dateStarted
-        elif willStart == 'N' or willStart == 'n':              # Else,
-            dateStarted = 'NULL'.strip(' ')                     # assume dateStarted will be in the future and return string 'NULL'
+        elif willStart == 'N' or willStart == 'n':                              # Else,
+            dateStarted = 'NULL'.strip(' ')                                     # assume dateStarted will be in the future and return string 'NULL'
             return dateStarted
         else:
-            print('Invalid input! Try again.')                  # If input is invalid, ask for input again
+            print('Invalid input! Try again.')                                  # If input is invalid, ask for input again
 
-def inputCategory(cursor):                                      # Function to ask user input for category name
-    while(1):                                                   # Infinite loop
+def inputCategory(cursor):                                                      # Function to ask user input for category name
+    while(1):                                                                   # Infinite loop
         categoryName = input("\nWhat category is your task in? Input: ")
         cursor.execute('SELECT categoryname FROM category')
 
-        for category in cursor:                                 # If category exists,
-            if categoryName in category:                        # return category name
+        for category in cursor:                                                 # If category exists,
+            if categoryName in category:                                        # return category name
                 return "'"+categoryName+"'"
 
-        print('Category does not exist!')                       # Else ask for input again 
-                                                                # TODO: Ask user if they want to create said category that does not exist
+        print('Category does not exist!')                                       # Else ask for input again 
+                                                                                # TODO: Ask user if they want to create said category that does not exist
 
-def addTask(cursor):                                            # Function for adding a task to the database
+def addTask(cursor):                                                            # Function for adding a task to the database
     clearTerminal()
 
     taskId = generateTaskId(cursor)
