49f25d9b38b08524a485dbbac94ff1125cf36297
andreapongco <aspongco@up.edu.ph>
2022-06-04 23:01:35 +0800

    UPDATE ON data.sql FILE

--- data.sql
+++ data.sql
@@ -2,15 +2,6 @@ DROP DATABASE IF EXISTS `app_data`;
 CREATE DATABASE IF NOT EXISTS `app_data`;
 USE `app_data`;
 
-CREATE TABLE IF NOT EXISTS `user` (
-    username        VARCHAR(20) NOT NULL,
-    contactnumber   VARCHAR(10)     NOT NULL,
-    fname           VARCHAR(20) NOT NULL,
-    mname           VARCHAR(20) NOT NULL,
-    lname           VARCHAR(20) NOT NULL,
-    PRIMARY KEY (`username`)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 CREATE TABLE IF NOT EXISTS `category` (
     categoryname  VARCHAR(20) NOT NULL,
     datecreated   DATE        NOT NULL,
@@ -32,16 +23,7 @@ CREATE TABLE IF NOT EXISTS `task` (
         FOREIGN KEY (`categoryname`) REFERENCES `category` (`categoryname`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `user_task` (
-    username        VARCHAR(20) NOT NULL,
-    taskid          VARCHAR(20) NOT NULL,
-    KEY `user_task_username_fk` (`username`),
-    KEY `user_task_taskid_fk` (`taskid`),
-    CONSTRAINT `user_task_username_fk` 
-        FOREIGN KEY (`username`) REFERENCES `user` (`username`),
-    CONSTRAINT `user_task_taskid_fk` 
-        FOREIGN KEY (`taskid`) REFERENCES `task` (`taskid`)
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 INSERT INTO category VALUES ('academics', CURDATE());
 INSERT INTO category VALUES ('chores', CURDATE());
